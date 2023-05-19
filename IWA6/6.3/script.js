eab4eb733d0368ffe63bd7ce646c510183ae1f99
Zmabuya12 <128055084+Zmabuya12@users.noreply.github.com>
2023-05-19 12:45:03 +0200

    Add files via upload

---
+++ IWA6/6.3/script.js
@@ -0,0 +1,8 @@
+const primaryPhone= '0748105141'
+const secondaryPhone= '0219131568'
+
+const primaryValid= typeof String== primaryPhone
+const secondaryValid= typeof String== secondaryPhone
+
+console.log(primaryValid)
+console.log(secondaryValid)
---
+++ IWA6/6.4/script.js
@@ -0,0 +1,18 @@
+const rent= 400;
+const tax= '8%';
+const food=51.7501;
+const salary= 800;
+const transport= 10.2;
+const hourofDay= 00;
+const minuteofDay= 00;
+
+if (hourofDay && minuteofDay !== null && hourofDay == '00' && minuteofDay == '00') {
+    const taxAsDecimal = tax / 100
+    const startingAfterTax = salary * 1 - taxAsDecimal
+    const balance = starting - transport - rent
+    const value = 'R'
+    console.log('balance')
+}
+else (hourofDay && minuteofDay !== unknown) 
+    console.log(undefined)
+
